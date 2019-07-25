018d352eb112aea7ca0e21205232710cafe2c3c9
Oleg_hos <ooleguner@gmail.com>
2019-07-25 18:03:49 +0300

    3

--- src/main/java/lesson3_HW/Controller/StorageController.java
+++ src/main/java/lesson3_HW/Controller/StorageController.java
@@ -1,7 +1,26 @@
 package lesson3_HW.Controller;
 
+import lesson3_HW.Beans.Storage;
+
+import javax.swing.plaf.synth.SynthToolTipUI;
+import java.util.List;
+
 /**
  * Created by oleg on 25.07.2019.
  */
 public class StorageController {
+
+    List<Storage> storageList;
+
+    public void addStorage(Storage storage){
+        storageList.add(storage);
+    }
+    public void deleteStorage(Storage storage){
+        storageList.remove(storage);
+    }
+    public List<Storage> getStorageList(){
+        return storageList;
+    }
+
+
 }
