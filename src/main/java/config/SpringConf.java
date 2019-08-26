15885514b4426b2ab032d8b573d27dc35e21bd7a
Oleg_hos <ooleguner@gmail.com>
2019-08-26 17:07:58 +0300

    1

--- src/main/java/config/SpringConf.java
+++ src/main/java/config/SpringConf.java
@@ -1,13 +1,14 @@
 package config;
 
 import lesson1.com.Student;
-import lesson1.com.Test;
+import lesson2_1.Route;
+import lesson2_1.Service;
+import lesson2_1.Step;
+import lesson2_2.repository.ItemDao;
+import lesson2_2.service.ItemService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,47 +19,61 @@ import java.util.Arrays;
 
 
 @Configuration
-@ComponentScan(basePackages = "lesson1.com")
+//@ComponentScan(basePackages = "lesson1.com")
+@ComponentScan(basePackages = "lesson2_2")
 public class SpringConf {
-
+    /*
+        lesson1
+     */
     @Bean
-    public Student student(){
+    public Student student() {
         return new Student();
     }
 
+    /*
+        lesson2_1
+     */
+    @Bean
+    public Service service1() {
+        return new Service(1L, "NameService1", new ArrayList(Arrays.asList("Param1_1", "Param1_2")));
+    }
 
+    @Bean
+    public Service service2() {
+        return new Service(2L, "NameService2", new ArrayList(Arrays.asList("Param2_1", "Param2_2")));
+    }
 
-//    @Bean
-//    public Test Test(){
-//        return new Test();
-//    }
-//    @Bean
-//    public Service service1() {
-//      return new Service(1L,"NameService1",new ArrayList(Arrays.asList("Param1_1", "Param1_2")));
-//    }
-//    @Bean
-//    public Service service2() {
-//        return new Service(2L,"NameService2",new ArrayList(Arrays.asList("Param2_1", "Param2_2")));
-//    }
-//
-//    @Bean
-//    public Service service3() {
-//        return new Service(2L,"NameService3",new ArrayList(Arrays.asList("Param3_1", "Param3_2")));
-//    }
-//
-//    @Bean
-//    public Step step1() {
-//        return new Step(100L, service1(),service2());
-//    }
-//
-//    @Bean
-//    public Step step2() {
-//        return new Step(101L, service2(),service2());
-//    }
-//
-//    @Bean
-//    public Route route(){
-//        return new Route("1000", Arrays.asList(step1(), step2()));
-//    }
+    @Bean
+    public Service service3() {
+        return new Service(2L, "NameService3", new ArrayList(Arrays.asList("Param3_1", "Param3_2")));
+    }
+
+    @Bean
+    public Step step1() {
+        return new Step(100L, service1(), service2());
+    }
+
+    @Bean
+    public Step step2() {
+        return new Step(101L, service2(), service2());
+    }
+
+    @Bean
+    public Route route() {
+        return new Route();
+    }
+
+    /*
+        lesson2_2
+     */
+    @Bean
+    public ItemService itemService() {
+        return new ItemService();
+    }
+    @Bean
+    public ItemDao itemDao(){
+        return new ItemDao();
+    }
+
 
 }
